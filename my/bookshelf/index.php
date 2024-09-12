1e3a90126867a62204175c2e8e226996d9cacc11
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-09-13 06:59:01 +0900

    myページやその他もろもろ

---
+++ my/bookshelf/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!--基本情報-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+        <title>マイページ - リレショウ!</title>
+        <link rel="icon" type="image/png" href="./logo.png">
+        <meta name="description" content="私の本棚">
+        <!--css-->
+        <link rel="stylesheet" href="./bookshelf.css">
+        <!--検索ボタン用CSS-->
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    </head>
+	<body>
+    <header>
+			<div>
+				<h1><img src="./dummy.jpg" alt="リレショウ!" class="logo"></h1>
+            </div>
+            <form action="" id="search_form">
+                <input type="search" class="text" placeholder="ユーザー名・キーワードで検索" value=""><input type="submit" class="search_button" value="&#xf002;">
+            </form>
+            <button id="search_face_button">&#xf002;検索</button>
+     	</header>
+        <ul>
+            <li>
+                <img src="" alt="">
+                <h3></h3>
+                <p></p>
+                
+            </li>
+        </ul>
+    </body>
+</html>
